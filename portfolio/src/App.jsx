c8ee0344214fd22357bd2af932a8b1af05ad7784
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-05-26 00:42:19 -0300

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/styles.css'
+
+function App() {
+  
+
+  return (
+      <div className="container">
+          <nav className='menu'>
+            <span>
+              Mi portfolio
+            </span>
+            <ul>
+              <li>Presentación</li>
+              <li>Proyectos</li>
+              <li>Contacto</li>
+            </ul>
+          </nav>
+      </div>
+  )
+}
+
+export default App
