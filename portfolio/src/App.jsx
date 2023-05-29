92923da544f123eb7caa5d7d8efdd11b28cd0956
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-05-28 21:05:35 -0300

    second commit
    monitor

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -7,18 +7,23 @@ function App() {
   
 
   return (
-      <div className="container">
+    <div className="container">
           <nav className='menu'>
-            <span>
-              Mi portfolio
-            </span>
-            <ul>
-              <li>Presentación</li>
-              <li>Proyectos</li>
-              <li>Contacto</li>
-            </ul>
+              <span>
+                Mi portfolio
+              </span>
+              <ul>
+                <li>Presentación</li>
+                <li>Proyectos</li>
+                <li>Galería</li>
+                <li>Contacto</li>
+              </ul>
+            
+            <div className="caja">
+
+            </div>
           </nav>
-      </div>
+    </div>
   )
 }
 
